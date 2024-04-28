6c2722046e6e431fb54c2bf2e928c9533cc538d8
codelenov <codelenovo@tutanota.com>
2024-04-28 19:13:41 +0200

    ADD for functional for calculate simple operations (+, -, *, -) with buttons `equals`. And ADD functional for clean divOutput with new calculations

--- script.js
+++ script.js
@@ -20,10 +20,106 @@ const square = document.querySelector("#square");
 const exponent = document.querySelector("#exponent");
 const pi = document.querySelector("#pi");
 const tau = document.querySelector("#tau");
-const equals = document.querySelector("#equals");
+
 const undo = document.querySelector("#undo");
+
+let buttons = document.querySelectorAll(".buttons");
+let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operators");
+const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
 
-let numberFirst = "";
-let numberSecond = "";
-let operator = "";
+let divOutput = document.querySelector("#divOutput");
+
+let inputAllString = ""; // all inputs (e.g. numbers and operators) are the string
+let inputArray = []; // for 'inputAllString' array
+let inputDigits = 0; // for calculate digits on screen (divOutput)
+const INPUTLENGTH = 16;
+let inputFirst = "";
+let inputSecond = "";
+let inputOperator = "";
+let result = "";
+
+getInput(); // program initialization
+
+function getInput() {
+
+	clear.addEventListener('click', () => {
+		divOutput.innerHTML = "";
+	}); 
+
+	buttons.forEach(function(b) {
+		b.addEventListener('click', () => {
+			inputAllString += b.innerHTML;
+			divOutput.innerHTML = inputAllString; // clean for new
+					console.log(inputAllString);
+		});
+	});
+
+	equals.addEventListener('click', () => {
+
+		inputAllString = inputAllString.slice(0, -1);
+
+		if (/\+/.test(inputAllString) === true) {
+			inputArray = inputAllString.split('+');
+			result = +inputArray[0] + +inputArray[1];
+		} else if (/\-/.test(inputAllString) === true) {
+			inputArray = inputAllString.split('-');
+			result = +inputArray[0] - +inputArray[1];
+		} else if (/\*/.test(inputAllString) === true) {
+			inputArray = inputAllString.split('*');
+			result = +inputArray[0] * +inputArray[1];
+		} else if (/\//.test(inputAllString) === true) {
+			inputArray = inputAllString.split('/');
+			result = +inputArray[0] / +inputArray[1];
+		}
+				console.log(inputAllString);
+				console.log(inputArray);
+				console.log(result);		
+		divOutput.innerHTML = result;
+		inputAllString = ""; // clean for new
+		inputArray = []; // clean for new
+	});
+
+}
+
+/* function getInput() {
+
+	clear.addEventListener('click', () => {
+		divOutput.innerHTML = "";
+		inputFirst = "";
+		inputSecond = "";
+		inputOperator = "";
+	});
+
+	numbers.forEach(function(n) {
+		n.addEventListener('click', () => {
+			divOutput.innerHTML += n.innerHTML;
+			inputFirst += n.value;
+		});
+	});
+
+operators.forEach(function(n) {
+		n.addEventListener('click', () => {
+			divOutput.innerHTML = n.innerHTML;
+			inputOperator = n.value;
+		});
+	});
+
+	numbers.forEach(function(n) {
+		n.addEventListener('click', () => {
+
+			divOutput.innerHTML += n.innerHTML;
+			inputSecond += n.value;
+		});
+	});	
+
+}
+
+function calculateResult() {
+	equals.addEventListener('click', () => {
+		let resultString = inputFirst + operator + inputSecond;
+		result = +resultString;
+		divOutput.innerHTML = result;
+	});
+} */
