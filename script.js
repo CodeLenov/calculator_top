7682ad99ab37b63271f2f18f1e5ead40248332bd
codelenov <codelenovo@tutanota.com>
2024-05-02 18:37:07 +0200

    FIX a bug '%' calculating (and edit some another code) in '%' calculating method replace() changed _only_first_ match, but I need change only _last_ match

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ clearInput();
 	undo.addEventListener('click', () => {
 		undo.value = "";
 		inputAll = inputAll.slice(0, -1);
-		/*divOutput.innerHTML = inputAll;*/
 	});
 }
 function clearInput() {
@@ -30,75 +29,86 @@ function clearInput() {
 		inputAll = "";
 	});
 }
-document.addEventListener('keydown', (b) => {
+
+document.addEventListener('keypress', (b) => {
 	if (b.key == '0') {b.value = '0'}
-	else if (b.key == '1') {b.value = '1'}
-	else if (b.key == '2') {b.value = '2'}
-	else if (b.key == '3') {b.value = '3'}
-	else if (b.key == '4') {b.value = '4'}
-	else if (b.key == '5') {b.value = '5'}
-	else if (b.key == '6') {b.value = '6'}
-	else if (b.key == '7') {b.value = '7'}
-	else if (b.key == '8') {b.value = '8'}
-	else if (b.key == '9') {b.value = '9'}
-	else if (b.key == '.') {b.value = '.'}
-	else if (b.key == '%') {b.value = '%'}
-	else if (b.key == '+') {b.value = '+'}
-	else if (b.key == '-') {b.value = '-'}
-	else if (b.key == '*') {b.value = '*'}
-	else if (b.key == '/') {b.value = '/'}
-	else if (b.key == 'p') {b.value = '\u03C0'}
-	else if (b.key == 't') {b.value = '\u03C4'}
-	else if (b.key == 'r') {b.value = '\u221A'}
-	else if (b.key == 's') {b.value = '\u2036'}
-	else if (b.key == 'm') {b.value = 'm'}
-	else if (b.key == 'e') {b.value = '^'}
-	else if (b.key == 'Backspace') {b.value = ''; undoInput();}
-	else if (b.key == '2') {b.value = '2'}
-	else if (b.key == '2') {b.value = '2'}
+	else if (b.key == '1') {b.value = '1'; inputAll += b.value;}
+	else if (b.key == '2') {b.value = '2'; inputAll += b.value;}
+	else if (b.key == '3') {b.value = '3'; inputAll += b.value;}
+	else if (b.key == '4') {b.value = '4'; inputAll += b.value;}
+	else if (b.key == '5') {b.value = '5'; inputAll += b.value;}
+	else if (b.key == '6') {b.value = '6'; inputAll += b.value;}
+	else if (b.key == '7') {b.value = '7'; inputAll += b.value;}
+	else if (b.key == '8') {b.value = '8'; inputAll += b.value;}
+	else if (b.key == '9') {buttons.value = '9'; printInput();}
+	else if (b.key == '.') {b.value = '.';}
+	else if (b.key == '%') {b.value = '%';}
+	else if (b.key == '+') {b.value = '+'; inputAll += b.value;}
+	else if (b.key == '-') {buttons.value = '-'; printInput();}
+	else if (b.key == '*') {b.value = '*';}
+	else if (b.key == '/') {b.value = '/';}
+	else if (b.key == 'p') {b.value = '\u03C0';}
+	else if (b.key == 't') {b.value = '\u03C4';}
+	else if (b.key == 'r') {b.value = '\u221A';}
+	else if (b.key == 's') {b.value = '\u2036';}
+	else if (b.key == 'm') {b.value = 'm';}
+	else if (b.key == 'e') {b.value = '^';}
+	else if (b.key == 'Backspace') {b.value = ''; inputAll = inputAll.slice(0, -1);;}
+	else if (b.key == 'Delete') {b.value = ''; inputAll = "";}
+	else if (b.key == 'Enter') {buttons.value = '=';}
 	else {b.value = ''}
 
 
-			inputAll += b.value;
-	printInput();			
-
+			/*inputAll += b.value;*/
+	
 });
+function printInput() {
+
+	divOutput.style.color = `#555`; // change color for input
+			divOutput.innerHTML = inputAll.slice(-16); // restrict maximal length divOutput
+
+}
 
 	buttons.forEach(function(b) {
 
 		b.addEventListener('click', () => {
-inputAll += b.value;
-			printInput();
+			inputAll += b.value;
+printInput();
 		console.log(33, inputAll);
 		});
 
 	});
 
-	function printInput() {
-	divOutput.style.color = `#555`; // change color for input
-			
-			divOutput.innerHTML = inputAll.slice(-16); // restrict maximal length divOutput
-}
+	
 
-
-	equals.addEventListener('click', () => {
+calculateResult();
+	equals.addEventListener('click', calculateResult);
+function calculateResult() {
 console.log(inputAll);
 		inputAll = inputAll.replace(`\u03C0`, `${Math.PI}`); // 'pi' button
 		inputAll = inputAll.replace(`\u03C4`, `${Math.PI * 2}`); // 'tau' button
 
+
 		if (/\%/.test(inputAll) === true) {
 
 			const percentAll = inputAll.slice(0, -2); // for remove equals sign and percent sign) from input string
+			
 			const percentArray = percentAll.split(/[^0-9]/);
+			
 			const percent = +percentArray[0] * (+percentArray[1] / 100);
+		
+			
+const lastIndex = inputAll.lastIndexOf(`${percentArray[1]}`);
 
-			inputAll = inputAll.replace(`${percentArray[1]}`, `${percent}`);
+inputAll = inputAll.slice(0, lastIndex) + percent + inputAll.slice(lastIndex + 1);
+
+
 			inputAll = inputAll.slice(0, -1); // for remove equals sign from input string
 
 		}
 
 	inputAll = inputAll.slice(0, -1); // for remove equals sign (or percent sign if it is last character) from input string
-
+console.log(60, inputAll);
 		// use regex for recognize operators
 		// split array with the operators for get first input number and second input number
 		// and change string type (elements of array) on integer type for calculating
@@ -132,6 +142,6 @@ console.log(000, result);
 		divOutput.innerHTML = result;
 		inputAll = ""; // clean for new input
 
-	});
-
+	
+}
 }
