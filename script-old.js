fd1ce776dcb9e25b79f233f2dfaa4565df4f6da8
codelenov <codelenovo@tutanota.com>
2024-05-05 17:47:45 +0200

    CHANGE calculateResult and some another code

--- script-old.js
+++ script-old.js
@@ -5,20 +5,11 @@ const undo = document.querySelector("#undo");
 const clear = document.querySelector("#clear");
 const equals = document.querySelector("#equals");
 
-let inputAll = ''; // all inputs (e.g. numbers and operators) are the string
+let inputAll = ""; // all inputs (e.g. numbers and operators) are the string
 let inputArray = []; // for 'inputAll' array
-let result = 0;
-let operator = '+';
-
-let inputNumber = '';
+let result = '';
 
 getInput(); // initialize the program
-
-// do 'point', 'percent' and others
-//do buttons, e.g. '=', 'undo', 'clear', 'point', 'percent' and others
-//    do document.addEventListener('keyup') for equals and operators (because normal innerHTML for operators)
-// <=16 digit input and output
-// clean unuse let and const
 
 function getInput() {
 
@@ -35,25 +26,25 @@ function getInput() {
 		else if (b.key == '8') {b.value = '8'; getKeyInput();}
 		else if (b.key == '9') {b.value = '9'; getKeyInput();}
 		else if (b.key == '%') {b.value = '%'; getKeyInput();}
-		else if (b.key == '+') {b.value = '+'; calculateResult(); operator = b.value;}
-		else if (b.key == '-') {b.value = '-'; calculateResult(); operator = b.value;}
-		else if (b.key == '*') {b.value = '*'; calculateResult(); operator = b.value;}
-		else if (b.key == '/') {b.value = '/'; calculateResult(); operator = b.value;}
+		else if (b.key == '+') {b.value = '+'; getKeyInput();}
+		else if (b.key == '-') {b.value = '-'; getKeyInput();}
+		else if (b.key == '*') {b.value = '*'; getKeyInput();}
+		else if (b.key == '/') {b.value = '/'; getKeyInput();}
 		else if (b.key == 'p') {b.value = '\u03C0'; getKeyInput();}
 		else if (b.key == 't') {b.value = '\u03C4'; getKeyInput();}
 		else if (b.key == 'r') {b.value = '\u221A'; getKeyInput();}
 		else if (b.key == 's') {b.value = '\u2036'; getKeyInput();}
 		else if (b.key == 'm') {b.value = 'm'; getKeyInput();}
 		else if (b.key == 'e') {b.value = '^'; getKeyInput();}
-		else if (b.key == 'Backspace') {b.value = ''; inputNumber = inputNumber.slice(0, -1); getKeyInput();}
-		else if (b.key == 'Delete') {b.value = ''; inputNumber = ''; operator = '+'; result = 0; getKeyInput()}
-		else if (b.key == 'Enter') {b.value = '='; calculateResult();}
+		else if (b.key == 'Backspace') {b.value = ''; inputAll = inputAll.slice(0, -1); getKeyInput();}
+		else if (b.key == 'Delete') {b.value = ''; inputAll = ""; getKeyInput();}
+		else if (b.key == 'Enter') {b.value = '='; inputAll += b.value; calculateResult();}
 		else {b.value = ''}
 
-		function getKeyInput() {
-			inputNumber += b.value;
+		function getKeyInput() { // inner function in document.addEventListener
+			inputAll += b.value;
 			divOutput.style.color = `#555`; // change color for input
-			divOutput.innerHTML = inputNumber.slice(-16); // restrict maximal length divOutput
+			divOutput.innerHTML = inputAll.slice(-16); // restrict maximal length divOutput
 		}
 
 	});
@@ -84,28 +75,8 @@ function getInput() {
 
 function calculateResult() {
 
-	if (operator === '+') {
-		result = +result + +inputNumber;
-	} else if (operator === '-') {
-		result = +result - +inputNumber;
-	} else if (operator === '*') {
-		result = +result * +inputNumber;
-	} else {
-		result = +result / +inputNumber;
-	}
-		
-	divOutput.style.color = `#222`;
-	divOutput.innerHTML = result;
+	inputAll = inputAll.slice(0, -1); // for remove equals sign from input string
 
-	inputNumber = '';
-
-}
-
-/*
-function calculateResult() {
-
-	inputAll = inputAll.slice(0, -1); // for remove equals/operator sign from input string
-console.log(700, inputAll);
 	inputAll = inputAll.replace(`\u03C0`, `3.14`); // if 'pi' button
 	inputAll = inputAll.replace(`\u03C4`, `3.14 * 2`); // if 'tau' button
 
@@ -116,32 +87,20 @@ console.log(700, inputAll);
 		// need round long decimals for the next check the length of the data for divOutput
 		percent = Math.round((percent + Number.EPSILON) * 100) / 100;
 		// need change 'inputArray[1]' on 'percent' for next calculating, because 'replace' work only with first value			
-		const lastIndexPercent = inputAll.lastIndexOf(`${percentArray[1]}`); 
-		inputAll = inputAll.slice(0, lastIndexPercent) + percent;
+		const lastIndex = inputAll.lastIndexOf(`${percentArray[1]}`); 
+		inputAll = inputAll.slice(0, lastIndex) + percent;
 	}
 
 	// use regex for recognize operators
 	// split array with the operators for get first input number and second input number
 	// and change string type (elements of array) on integer type for calculating
 	if (/\+/.test(inputAll) === true) {
-		/*inputArray = inputAll.split('+');
-const inputAllLastIndex = inputAll.lastIndexOf(/[^0-9.]/);
-		console.log(701, inputAllLastIndex);
-inputArray[0] = inputAll.slice(0, inputAllLastIndex - 1);
-inputArray[1] = inputAll.slice(inputAllLastIndex);
-console.log(702, inputArray[0]);
-console.log(703, inputAllLastIndex[1]);
+		inputArray = inputAll.split('+');
 		result = +inputArray[0] + +inputArray[1];
 	} else if (/\-/.test(inputAll) === true) {
-		/*inputArray = inputAll.split('-');
-		const inputAllLastIndex = inputAll.lastIndexOf(/[^0-9.]/);
-		console.log(701, inputAllLastIndex);
-inputArray[0] = inputAll.slice(0, inputAllLastIndex - 1);
-inputArray[1] = inputAll.slice(inputAllLastIndex);
-console.log(702, inputArray[0]);
-console.log(703, inputAllLastIndex[1]);
+		inputArray = inputAll.split('-');
 		result = +inputArray[0] - +inputArray[1];
-	} else if (/\*//*.test(inputAll) === true) {
+	} else if (/\*/.test(inputAll) === true) {
 		inputArray = inputAll.split('*');
 		result = +inputArray[0] * +inputArray[1];
 	} else if (/\//.test(inputAll) === true) {
@@ -168,16 +127,14 @@ console.log(703, inputAllLastIndex[1]);
 	result = result + '';
 
 	divOutput.style.color = `#222`; // change color for result
-	divOutput.innerHTML = result;
-		inputAll = '';
-	/* // check the length of the data for divOutput
+	
+	// check the length of the data for divOutput
 	if ( (inputArray[0].length < 16) && (inputArray[1].length < 16) && (result.length < 16) ) {
 		divOutput.innerHTML = result;
 		inputAll = ''; // clean for new input
 	} else {
 		divOutput.innerHTML = 'so much long...>16'
 		inputAll = ''; // clean for new input
-	} 
+	}
 
 }
-*/
